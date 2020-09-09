20b3bd2af51765e8f8e66b2d547967eb253ee35b
Allen <allenvs@mail.ru>
2020-09-09 20:26:08 +0300

    setup added

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+console.log(longestCommonPrefix(["flower","flow","flight"]));
+
+function longestCommonPrefix(strs) {
+    let prefix = "";
+    return prefix; 
+};
